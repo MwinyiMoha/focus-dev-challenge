ec4ae37cdb663b30592373b5b71550ac5eb5074f
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2025-12-03 13:03:48 +0300

    Update config for running worker process

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,10 +8,13 @@ import (
 
 type Config struct {
 	ServiceName    string `mapstructure:"SERVICE_NAME" validate:"required"`
+	AppTier        string `mapstructure:"APP_TIER" validate:"required,oneof=web worker"`
 	Debug          bool   `mapstructure:"DEBUG"`
 	DefaultTimeout int    `mapstructure:"DEFAULT_TIMEOUT" validate:"required,gt=0"`
 	ServerPort     int    `mapstructure:"SERVER_PORT" validate:"required"`
 	DatabaseURL    string `mapstructure:"DATABASE_URL" validate:"required"`
+	RedisHost      string `mapstructure:"REDIS_HOST"`
+	RedisDB        int    `mapstructure:"REDIS_DB"`
 }
 
 func New(val *validator.Validate) (*Config, error) {
@@ -19,10 +22,13 @@ func New(val *validator.Validate) (*Config, error) {
 	v.SetConfigType("env")
 
 	v.SetDefault("SERVICE_NAME", "")
+	v.SetDefault("APP_TIER", "web")
 	v.SetDefault("DEBUG", true)
 	v.SetDefault("DEFAULT_TIMEOUT", 10)
 	v.SetDefault("SERVER_PORT", 8080)
 	v.SetDefault("DATABASE_URL", "")
+	v.SetDefault("REDIS_HOST", "")
+	v.SetDefault("REDIS_DB", 0)
 
 	v.AutomaticEnv()
 
@@ -38,6 +44,7 @@ func New(val *validator.Validate) (*Config, error) {
 		return nil, errors.WrapError(err, errors.Internal, "failed to unmarshal config")
 	}
 
+	val.RegisterStructValidation(validateWorkerTier, Config{})
 	if err := cfg.validate(val); err != nil {
 		return nil, err
 	}
@@ -51,4 +58,14 @@ func (c *Config) validate(v *validator.Validate) error {
 	}
 
 	return nil
+}
+
+func validateWorkerTier(sl validator.StructLevel) {
+	cfg := sl.Current().Interface().(Config)
+
+	if cfg.AppTier == "worker" {
+		if cfg.RedisHost == "" {
+			sl.ReportError(cfg.RedisHost, "RedisHost", "RedisHost", "required_for_worker_tier", "")
+		}
+	}
 }
