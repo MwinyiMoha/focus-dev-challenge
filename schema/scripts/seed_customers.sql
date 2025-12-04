2233e7e83e74628d1e6d3580c14b85ac5800d4cf
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2025-12-04 09:14:14 +0300

    Add seed data for campaigns & customers

--- schema/scripts/seed_customers.sql
+++ schema/scripts/seed_customers.sql
@@ -3,3 +3,28 @@ values('+254712832088', 'Mohammed', 'Mwijaa', 'Mombasa', 'White Sneakers');
 
 insert into customers (phone, first_name, last_name, "location", preferred_product)
 values('+254726208234', 'John', 'Doe', 'Nairobi', 'Grey Hoodies');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254700111222', 'Alice', 'Kamau', 'Kisumu', 'Blue Jeans');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254711333444', 'Brian', 'Otieno', 'Nakuru', 'Leather Jacket');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254722555666', 'Catherine', 'Awuor', 'Eldoret', 'Running Shoes');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254733777888', 'David', 'Kilonzo', 'Thika', 'Baseball Cap');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254744999000', 'Evelyn', 'Njeri', 'Nanyuki', 'Sun Dress');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254755121314', 'Frank', 'Mugo', 'Malindi', 'Beach Shorts');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254766151617', 'Grace', 'Wanjiru', 'Naivasha', 'Formal Shirt');
+
+insert into customers (phone, first_name, last_name, "location", preferred_product)
+values('+254777181920', 'Henry', 'Mwangi', 'Kitale', 'Wool Sweater');
+
